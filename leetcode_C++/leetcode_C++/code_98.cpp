0070975084ef7c81999aad4cadea4b89a97fc405
hapii <869932084@qq.com>
2019-12-30 22:05:48 +0800

    验证二叉搜索树

--- leetcode_C++/leetcode_C++/code_98.cpp
+++ leetcode_C++/leetcode_C++/code_98.cpp
@@ -8,9 +8,44 @@
 
 #include <stdio.h>
 
+/*
+ 给定一个二叉树，判断其是否是一个有效的二叉搜索树。
+ 
+ 假设一个二叉搜索树具有如下特征：
+ 
+ 节点的左子树只包含小于当前节点的数。
+ 节点的右子树只包含大于当前节点的数。
+ 所有左子树和右子树自身必须也是二叉搜索树。
+ 示例 1:
+
+ */
+
+struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+ 
 class code_98 {
     
     
 public:
-    
+    TreeNode* pre;
+    bool isValidBST(TreeNode* root) {
+        if (root == nullptr) {
+            return true;
+        }
+        if(!isValidBST(root->left)){
+            return false;
+        }
+        if(pre && pre->val >= root->val){
+            return false;
+        }
+        pre = root;
+        if(!isValidBST(root->right)){
+            return false;
+        }
+        return true;
+    }
 };
